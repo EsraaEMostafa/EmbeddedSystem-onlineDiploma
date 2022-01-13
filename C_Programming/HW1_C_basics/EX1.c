783ae555463301b608203ec6f50afa76336884dd
Esraa Ebrahim <esraaebrahim1011@gmail.com>
2022-01-13 17:38:23 +0200

    Homework 1 for C basics lesson

---
+++ C_Programming/HW1_C_basics/EX1.c
@@ -0,0 +1,14 @@
+/*
+ * EX1.c
+ *
+ *  Created on: Jan 13, 2022
+ *      Author: LENOVO
+ */
+
+
+#include <stdio.h>
+
+int main ()
+{
+	printf("C Programming") ;
+}
---
+++ C_Programming/HW1_C_basics/EX3.c
@@ -0,0 +1,17 @@
+/*
+ * EX3.c
+ *
+ *  Created on: Jan 13, 2022
+ *      Author: LENOVO
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	int x , y  ;
+	printf("Enter two integers: ");
+	fflush(stdin);fflush(stdout);
+	scanf ("%d %d",&x,&y);
+	printf("Sum: %d",x+y);
+}
