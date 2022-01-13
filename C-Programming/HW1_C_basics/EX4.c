066f0fee7f9331b64016e7634e35995650cc0fbd
Esraa Ebrahim <esraaebrahim1011@gmail.com>
2022-01-13 17:49:28 +0200

    Homework 1 for c basics lesson

---
+++ C-Programming/HW1_C_basics/EX4.c
@@ -0,0 +1,17 @@
+/*
+ * EX4.c
+ *
+ *  Created on: Jan 13, 2022
+ *      Author: LENOVO
+ */
+#include <stdio.h>
+
+int main()
+{
+	float x , y  ;
+	printf("Enter two numbers: ");
+	fflush(stdin);fflush(stdout);
+	scanf ("%f %f",&x,&y);
+	printf("Product: %f",x*y);
+}
+
